635f773fdc70c7367c366220ea77a377f8886ef8
Mono <rickyblesstf@gmail.com>
2024-06-26 20:25:21 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from './imgs/logo.png';
+import MainImg from './imgs/img-lg.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="container">
+          <img src={Logo} alt="Logo do Restaurante" />
+          <div className='menu-hamburguer'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </header>
+      <section className='main-img'>
+        <img src={MainImg} alt="Imagem principal" />
+      </section>
     </div>
   );
 }
