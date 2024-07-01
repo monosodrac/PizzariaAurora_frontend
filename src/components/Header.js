641a62b9d24564cfb8a0e8924e010ef4c90e2ca6
Mono <rickyblesstf@gmail.com>
2024-07-01 10:52:59 -0300

    truly creating about section

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Logo from '../imgs/logo.png';
+import MainImg from '../imgs/img-lg.png';
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <img src={Logo} alt="Logo do Restaurante" />
+          <div className='menu-hamburguer'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </header>
+      <section className='main-img'>
+        <img src={MainImg} alt="Imagem principal" />
+      </section>
+    </div>
+  );
+}
