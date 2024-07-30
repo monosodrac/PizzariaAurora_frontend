3e6e274da1e4b04362d984ca564962ea848dbe54
monosodrac <monosodrac@gmail.com>
2024-07-30 20:56:43 -0300

    updating commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import Rotas from './Router';
-import CadProdutos from './Components/CadProdutos'
+import Rotas from './Router';
+// import CadProdutos from './Components/CadProdutos'
 
 import "./Styles/main.scss"; 
 
 export default function App() {
   return (
     <div>
-      {/* <Rotas /> */}
-      <CadProdutos />
+      <Rotas />
+      {/* <CadProdutos /> */}
     </div>
   );
 }
